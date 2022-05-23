b223d743dbfa401f2a263bd3a02ac5e3f8b93ad4
Sanjit <Sanjit.poudel28@gmail.com>
2022-05-23 14:32:42 +0200

    Unittesting

---
+++ Unittest/FootBallTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Unittest
+{
+    [TestClass]
+    public class FootBallTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
